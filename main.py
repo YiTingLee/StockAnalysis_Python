77374bc933c73b380b8ad13d313e76a4e3d2608e
YiTingLee <ranye0719@gmail.com>
2017-07-25 11:20:12 +0800

    add Candlestick Chart

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 import datetime
 from pandas_datareader import data
+from bokeh.plotting import figure, show, output_file
 
-start = datetime.datetime(2017,3,1)
-end = datetime.datetime(2017,3,10)
+def inc_dec(c,o):
+    if(c>o):
+        value="Increase"
+    elif(c<o):
+        value="Decrease"
+    else:
+        value="Equal"
+    return value
+
+start = datetime.datetime(2016,3,1)
+end = datetime.datetime(2016,3,10)
 
 df = data.DataReader(name="GOOG", data_source="google", start=start, end=end)
 
+df["Status"] = [inc_dec(c,o) for c,o in zip(df.Close, df.Open)]
+df["Middle"] = (df.Open+df.Close)/2
+df["Height"] = abs(df.Open-df.Close)
+
 print(df)
+
+# date_increase = df.index[df.Close > df.Open]
+# date_decrease = df.index[df.Close < df.Open]
+
+# print(date_increase)
+
+p = figure(x_axis_type='datetime', width=1000, height=300, title="Candlestick Chart")
+
+hours_12=12*60*60*1000
+
+p.rect(df.index[df.Status == "Increase"], df.Middle[df.Status=="Increase"], hours_12, df.Height[df.Status=="Increase"], fill_color='green', line_color='black')
+p.rect(df.index[df.Status == "Decrease"], df.Middle[df.Status=="Decrease"], hours_12, df.Height[df.Status=="Decrease"], fill_color='red', line_color='black')
+output_file("CS.html")
+show(p)
